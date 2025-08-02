00c916cf186cfa2ac605b6ef7b2ae429052e82b6
Samad126 <samed192005@gmail.com>
2025-08-02 23:16:21 +0400

    fix for CLS in why choose section

--- components/Home/FeatureCard.tsx
+++ components/Home/FeatureCard.tsx
@@ -9,7 +9,7 @@ type FeatureCardProps = {
 const FeatureCard = ({ Icon, title, desc }: FeatureCardProps) => (
   <div className="bg-gradient-to-br from-gray-800 to-gray-900 p-6 sm:p-8 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 border border-gray-700 hover:border-green-500/30 group">
     <div className="bg-gradient-to-br from-green-500 to-green-600 w-16 h-16 rounded-xl flex items-center justify-center mb-4 sm:mb-6 mx-auto group-hover:scale-110 transition-transform duration-300">
-      <Icon className="text-black text-2xl" />
+      <Icon className="w-6 h-6 text-black text-2xl" />
     </div>
     <h3 className="text-white text-lg sm:text-xl font-semibold mb-2 sm:mb-3">
       {title}
